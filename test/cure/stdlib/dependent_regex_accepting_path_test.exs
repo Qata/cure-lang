ee588fae4e16b7470eb73b66908039a9c9e62a35
Made In Heaven <madeinheaven@madeinheaven.com>
2026-08-11 08:53:49 +1000

    Classify concat left transitions

--- test/cure/stdlib/dependent_regex_accepting_path_test.exs
+++ test/cure/stdlib/dependent_regex_accepting_path_test.exs
@@ -789,6 +789,8 @@ defmodule Cure.Stdlib.DependentRegexAcceptingPathTest do
     assert Env.total?(env, :"Std.Regex.Proof#filtered_state_origin")
     assert Env.get_def(env, :"Std.Regex.Proof#filtered_continue_with_right_state_origin")
     assert Env.total?(env, :"Std.Regex.Proof#filtered_continue_with_right_state_origin")
+    assert Env.get_def(env, :"Std.Regex.Proof#concat_left_transition_origin")
+    assert Env.total?(env, :"Std.Regex.Proof#concat_left_transition_origin")
     assert Env.get_def(env, :"Std.Regex.Proof#project_alternate_left_acceptance")
     assert Env.total?(env, :"Std.Regex.Proof#project_alternate_left_acceptance")
     assert Env.get_def(env, :"Std.Regex.Proof#project_alternate_right_acceptance")
