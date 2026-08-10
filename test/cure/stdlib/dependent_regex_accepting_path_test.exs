9a422e85192cc622d3ba5e6a85bfead5b25d39c3
Made In Heaven <madeinheaven@madeinheaven.com>
2026-08-11 09:32:31 +1000

    Project concat left accepting paths

--- test/cure/stdlib/dependent_regex_accepting_path_test.exs
+++ test/cure/stdlib/dependent_regex_accepting_path_test.exs
@@ -809,6 +809,8 @@ defmodule Cure.Stdlib.DependentRegexAcceptingPathTest do
     assert Env.total?(env, :"Std.Regex.Proof#project_concat_left_handoff_active")
     assert Env.get_def(env, :"Std.Regex.Proof#project_concat_left_handoff_accepted")
     assert Env.total?(env, :"Std.Regex.Proof#project_concat_left_handoff_accepted")
+    assert Env.get_def(env, :"Std.Regex.Proof#project_concat_left_path")
+    assert Env.total?(env, :"Std.Regex.Proof#project_concat_left_path")
     assert Env.get_def(env, :"Std.Regex.Proof#project_alternate_left_acceptance")
     assert Env.total?(env, :"Std.Regex.Proof#project_alternate_left_acceptance")
     assert Env.get_def(env, :"Std.Regex.Proof#project_alternate_right_acceptance")
