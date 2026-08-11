92a5ac880a9ab7aaae609a21e5d9e1b4dc3db100
Made In Heaven <madeinheaven@madeinheaven.com>
2026-08-11 21:28:20 +1000

    Prove right alternative origin capture projection

--- test/cure/stdlib/dependent_regex_accepting_path_test.exs
+++ test/cure/stdlib/dependent_regex_accepting_path_test.exs
@@ -907,6 +907,8 @@ defmodule Cure.Stdlib.DependentRegexAcceptingPathTest do
     assert Env.total?(env, :"Std.Regex.Proof#project_alternate_left_acceptance")
     assert Env.get_def(env, :"Std.Regex.Proof#alternate_left_origin_acceptance_captures")
     assert Env.total?(env, :"Std.Regex.Proof#alternate_left_origin_acceptance_captures")
+    assert Env.get_def(env, :"Std.Regex.Proof#alternate_right_origin_acceptance_captures")
+    assert Env.total?(env, :"Std.Regex.Proof#alternate_right_origin_acceptance_captures")
     assert Env.get_def(env, :"Std.Regex.Proof#project_alternate_right_acceptance")
     assert Env.total?(env, :"Std.Regex.Proof#project_alternate_right_acceptance")
     assert Env.get_def(env, :"Std.Regex.Proof#project_alternate_acceptance")
