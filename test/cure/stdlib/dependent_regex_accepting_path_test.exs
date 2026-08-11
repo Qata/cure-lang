f7a3d8a85b61f020810d0da9e472bf41b3875066
Made In Heaven <madeinheaven@madeinheaven.com>
2026-08-11 16:47:55 +1000

    Share right alternate capture outputs

--- test/cure/stdlib/dependent_regex_accepting_path_test.exs
+++ test/cure/stdlib/dependent_regex_accepting_path_test.exs
@@ -790,6 +790,8 @@ defmodule Cure.Stdlib.DependentRegexAcceptingPathTest do
     assert Env.total?(env, :"Std.Regex.Proof#project_alternate_left_shared_execution")
     assert Env.get_def(env, :"Std.Regex.Proof#AlternateRightExecutedPathProjection")
     assert Env.total?(env, :"Std.Regex.Proof#project_alternate_right_executed_path")
+    assert Map.has_key?(env.ctors, :"Std.Regex.Proof#ProjectedAlternateRightSharedExecution")
+    assert Env.total?(env, :"Std.Regex.Proof#project_alternate_right_shared_execution")
     assert Map.has_key?(env.ctors, :"Std.Regex.Proof#ProjectedAlternateLeftExecutedAcceptance")
     assert Env.total?(env, :"Std.Regex.Proof#execute_alternate_left_acceptance_projection")
     assert Map.has_key?(env.ctors, :"Std.Regex.Proof#ProjectedAlternateRightExecutedAcceptance")
